1bc3dfaf44c7c4f4ba947872cdef36e7c5dc5707
Kartik <0912kartik@gmail.com>
2024-07-27 22:50:21 +0530

    Prompt creation and display

--- components/Form.jsx
+++ components/Form.jsx
@@ -25,14 +25,27 @@ const Form = ({type ,post ,setPost ,submitting ,handleSubmit}) => {
         </label>
 
         <label>
-          <span className="font-satoshi font-semibold text-base text-gray-700">Tag</span>
-          <textarea value={post.prompt} 
-          onChange={(e)=>setPost({...post, prompt: e.target.value })}
-          placeholder="Write your prompt here..."
-          className="form_textarea"
+          <span className="font-satoshi font-semibold text-base text-gray-700">Tag {' '}
+            <span className="font-normal">(#product,#webdevelopment,#idea)</span>
+          </span>
+          <input 
+          value={post.tag} 
+          onChange={(e)=>setPost({...post, tag: e.target.value })}
+          placeholder="#tag"
+          className="form_input"
           required>
-          </textarea>
+          </input>
         </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+        <Link href= "/" className="text-gray-500 text-sm">
+        Cancel
+        </Link>
+        <button type="submit" disabled={submitting} 
+        className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white">
+        {submitting ? `${type}...` : type}
+        </button>
+        </div>
       </form>
     </section>
   )
